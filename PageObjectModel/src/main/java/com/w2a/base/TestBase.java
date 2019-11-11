99f96fdc8137e849761f71ef4e781bebef35fd4c
arvind0604 <arvind6tomar@gmail.com>
2019-11-11 21:15:09 +0530

    first commit

---
+++ PageObjectModel/src/main/java/com/w2a/base/TestBase.java
@@ -0,0 +1,67 @@
+package com.w2a.base;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+public class TestBase {
+	
+	/*
+	 * Webdriver
+	 * Properties
+	 * Logs
+	 * Extent report
+	 * DB
+	 * Excel
+	 * Mail
+	 * 
+	 * 
+	 */
+
+	public static WebDriver driver;
+	public static Properties prop;
+	public static FileInputStream fis;
+	
+	@BeforeSuite
+	public void Setup() {
+		
+		if(driver==null) {
+			
+			prop=new Properties();
+			fis = null;
+			try {
+				fis = new FileInputStream(System.getProperty("user.dir")+"\\src\\test"
+						+ "\\resources\\properties\\config.properties");
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			try {
+				prop.load(fis);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			if(prop.getProperty("browser").equals("chrome")) {
+				System.getProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"\\chromedriver_win32\\chromedriver.exe" );
+				driver=new ChromeDriver();
+			}
+			
+		}
+	}
+	
+	
+	
+	@AfterSuite
+	public void TearDown() {
+		
+	}
+	
+}
---
+++ PageObjectModel/src/main/java/com/w2a/properties/TestProperties.java
@@ -0,0 +1,20 @@
+package com.w2a.properties;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class TestProperties {
+
+	public static void main(String[] args) throws IOException {
+		
+		Properties prop=new Properties();
+		FileInputStream fis=new FileInputStream(System.getProperty("user.dir")+"\\src\\test"
+				+ "\\resources\\properties\\config.properties");
+		prop.load(fis);
+		
+
+	}
+
+}
